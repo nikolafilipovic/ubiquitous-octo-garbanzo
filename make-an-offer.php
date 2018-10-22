a01ae9a1e4a651903249ea79d862a61201065302
milutinvelisic <44348167+milutinvelisic@users.noreply.github.com>
2018-10-22 12:34:58 +0200

    Added make an offer
    And a bunch of other crap

---
+++ make-an-offer.php
@@ -0,0 +1,16 @@
+<?php
+    /*
+        Template Name: Make an offer
+    */
+    get_header();
+?>
+<style>
+    .main-header {
+        background: blue;
+    }
+</style>
+<!-- Pisi HTML OVDE -->
+<h1>Neki levi tag</h1>
+<?php
+    get_footer();
+?>
