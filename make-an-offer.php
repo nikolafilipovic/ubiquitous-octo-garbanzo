09199ae28b9c5e411547e288e60844d588e0dee5
milutinvelisic <44348167+milutinvelisic@users.noreply.github.com>
2018-10-23 02:30:46 +0200

    20% of total work done
    6 psd-ova inicijalno za telefon zavrseno, sutra ce ostatak pa onda ide usavrsavanje detalja pa onda ostali uredjaji

--- make-an-offer.php
+++ make-an-offer.php
@@ -9,8 +9,179 @@
         background: blue;
     }
 </style>
-<!-- Pisi HTML OVDE -->
-<h1>Idemo</h1>
+   <div id="wrapper">
+    <div class="wrapperTop">
+      <a href="#"><p><i class="fa fa-angle-left" aria-hidden="true"></i> Back</p></a>
+    </div>
+    <div class="wrapperMid">
+        <h1>Make an offer on</h1>
+        <h2>11249 E Sonrisa Ave</h2>
+        <h3>Mesa, AZ 85212</h3>
+        <hr/>
+        <h4>Let's get started. Who is making the offer?</h4>
+    </div>
+    <div class="wrapperForm">
+      <div class="formBlock formBottomLine">
+        <div class="formCircle">
+
+        </div>
+        <h4>Buyer</h4>
+        <div class="formArrow">
+          <i class="fa fa-arrow-right" aria-hidden="true"></i>
+        </div>
+        <div class="cleaner">
+
+        </div>
+      </div>
+      <div class="formBlock formBottomLine">
+        <div class="formCircle">
+
+        </div>
+        <h4>Buyers Agent</h4>
+        <div class="formArrow">
+          <i class="fa fa-arrow-right" aria-hidden="true"></i>
+        </div>
+        <div class="cleaner">
+
+        </div>
+      </div>
+      <div class="formBlock">
+        <div class="formCircle">
+
+        </div>
+        <h4>Investor</h4>
+        <div class="formArrow">
+          <i class="fa fa-arrow-right" aria-hidden="true"></i>
+        </div>
+        <div class="cleaner">
+
+        </div>
+      </div>
+    </div>
+    <!-- Novi block -->
+    <div class="wrapperMid">
+        <h1>Are you working with a real estate agent?</h1>
+    </div>
+    <div class="wrapperForm">
+      <div class="formBlock formBottomLine">
+        <div class="formCircle">
+
+        </div>
+        <h4>Yes</h4>
+        <div class="formArrow">
+          <i class="fa fa-arrow-right" aria-hidden="true"></i>
+        </div>
+      </div>
+      <div class="formBlock formBottomLine">
+        <div class="formCircle">
+
+        </div>
+        <h4>No</h4>
+        <div class="formArrow">
+          <i class="fa fa-arrow-right" aria-hidden="true"></i>
+        </div>
+      </div>
+    </div>
+    <!-- Novi block -->
+    <div class="wrapperMid">
+        <h1>We can write up the offer for you.</h1>
+        <h5>Please provide the informaiton below and Annete will contact you within 5 minutes.</h5>
+    </div>
+    <div class="wrapperForm wrapperFormTransparent">
+      <form class="" action="#" method="post">
+        <div class="wrapperFormText">
+          <input type="text" name="Name" class="input1" value="Strahinja" placeholder="Name" />
+          <input type="text" name="Last Name" class="input2" value="" placeholder="Last name" />
+          <div class="cleaner">
+          </div>
+        </div>
+        <input type="number" class="formFullWidth" name="" value="" placeholder="Phone"/>
+        <input type="email" class="formFullWidth" name="" value="" placeholder="Email"/>
+        <input type="submit" class="formFullWidth formSubmit" name="submit" value="Submit">
+      </form>
+    </div>
+    <div class="personBlock">
+      <div class="iconBlock">
+
+      </div>
+      <div class="infoBlock">
+        <h6>Anette Brown</h6>
+        <p class="infoblockName">Licensed agent</p>
+        <span class="fa fa-star" aria-hidden="true"></span>
+        <span class="fa fa-star" aria-hidden="true"></span>
+        <span class="fa fa-star" aria-hidden="true"></span>
+        <span class="fa fa-star" aria-hidden="true"></span>
+        <span class="fa fa-star" aria-hidden="true"></span>
+        <p>602-448-7377</p>
+        <span>anette@theofferco.com</span>
+      </div>
+      <div class="cleaner">
+
+      </div>
+    </div>
+    <!-- Novi block -->
+    <div class="wrapperMid">
+      <h1>Success!</h1>
+      <h6>Anette Brown will contact you shortly.</h6>
+    </div>
+    <div class="personBlock">
+      <div class="iconBlock">
+
+      </div>
+      <div class="infoBlock">
+        <h6>Anette Brown</h6>
+        <p class="infoblockName">Licensed agent</p>
+        <span class="fa fa-star" aria-hidden="true"></span>
+        <span class="fa fa-star" aria-hidden="true"></span>
+        <span class="fa fa-star" aria-hidden="true"></span>
+        <span class="fa fa-star" aria-hidden="true"></span>
+        <span class="fa fa-star" aria-hidden="true"></span>
+        <p>602-448-7377</p>
+        <span>anette@theofferco.com</span>
+      </div>
+      <div class="cleaner">
+      </div>
+    </div>
+    <!-- Novi block -->
+    <div class="wrapperMid">
+      <h1>Invite your agent</h1>
+      <h6>We'll send your agent a link with your request to make an offer on this property.</h6>
+    </div>
+    <div class="wrapperForm wrapperFormTransparent">
+      <form class="" action="#" method="post">
+        <legend class="legend">Your informaiton</legend>
+        <div class="wrapperFormText">
+          <input type="text" name="Name" class="input1" value="Strahinja" placeholder="Name" />
+          <input type="text" name="Last Name" class="input2" value="" placeholder="Last name" />
+          <div class="cleaner">
+          </div>
+        </div>
+        <input type="email" class="formFullWidth" name="" value="" placeholder="Email"/>
+      </form>
+      <form class="" action="#" method="post">
+        <legend class="legend">Your agents informaiton</legend>
+        <div class="wrapperFormText">
+          <input type="text" name="Name" class="input1" value="" placeholder="Name" />
+          <input type="text" name="Last Name" class="input2" value="" placeholder="Last name" />
+          <div class="cleaner">
+          </div>
+        </div>
+        <input type="email" class="formFullWidth" name="" value="" placeholder="Email"/>
+        <input type="submit" class="formFullWidth formSubmit" name="submit" value="Submit">
+      </form>
+    </div>
+    <!-- Novi block -->
+    <div class="wrapperMid">
+      <h1>Success!</h1>
+      <h6>An email has been sent to your agent.</h6>
+    </div>
+    <div class="goBack">
+      <a href="#"><p><i class="fa fa-angle-left" aria-hidden="true"></i>Go back</p></a>
+    </div>
+
+
+  </div> <!-- Wrapper -->
+
 <?php
     get_footer();
 ?>
