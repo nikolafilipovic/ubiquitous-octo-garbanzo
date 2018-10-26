1e688ca7e50fe24bc08d67d16267423ae93e3f63
milutinvelisic <44348167+milutinvelisic@users.noreply.github.com>
2018-10-27 00:43:31 +0200

    Still re-doing
    ...

--- make-an-offer.php
+++ make-an-offer.php
@@ -99,9 +99,9 @@
             <input type="text" name="Name" class="formMR" value="Strahinja" placeholder="Name" />
           </div>
           <div class="wrapperFormText">
-            <input type="text" name="Last Name" class="input2" value="" placeholder="Last name" />
+            <input type="text" name="Last Name" class="" value="" placeholder="Last name" />
           </div>
-          <input type="text" class="formMR" name="" value="" placeholder="Phone"/>
+          <input type="text" class="formFullWidth formMR" name="" value="" placeholder="Phone"/>
           <input type="email" class="formFullWidth" name="" value="" placeholder="Email"/>
           <input type="submit" class="formSubmitFull" name="submit" value="Submit &rightarrow;">
         </form>
@@ -205,45 +205,43 @@
       <div class="wrapperMid wrapperMidSmall">
         <h1 class="hSmallMargin">Financial terms</h1>
       </div>
-      <div class="wrapperForm wrapperFormTransparent wrapperFormNothing">
-        <form class="" action="index.html" method="post">
-            <div class="formsLeft">
-                <div class="wrapperFormText">
-                    <legend >Purchase price</legend>
-                    <input type="number" class="formHalf" name="purPrice" value="" placeholder="$"/>
-                  </div>
-                  <div class="wrapperFormText">
-                    <legend >Earnest money</legend>
-                    <input type="number" class="formHalf" name="earnMoney" value="" placeholder="$"/>
-                  </div>
-                <div class="cleaner"></div>
+      <div class="wrapperForm  wrapperFormDekstop">
+        <form class="formsLeft " action="index.html" method="post">
+            <div class="wrapperFormText">
+                <legend class="legend">Purchase price</legend>
+                <input type="text" class="formMR" name="purPrice" value="" placeholder="$"/>
+              </div>
+            <div class="wrapperFormText">
+                 <legend class="legend">Earnest money</legend>
+                 <input type="text" class="" name="earnMoney" value="" placeholder="$"/>
             </div>
-            <div class="formsRight">
-                <div class="wrapperFormText">
-                    <legend >Down payment</legend>
-                    <input type="number" class="formHalf" name="purPrice" value="" placeholder="%" />
-                </div>
-                <div class="wrapperFormText">
-                    <legend >Loan amount</legend>
-                    <input type="number" class="formHalf" name="earnMoney" value="" placeholder="$"/>
-
-                </div>
-                <div class="cleaner"></div>
+        </form>
+        <form class="formsLeft">
+            <div class="wrapperFormText">
+                <legend class="legend">Down payment</legend>
+                <input type="text" class="" name="purPrice" value="" placeholder="%" />
             </div>
-          
-          <div class="wrapperForm wrapperFormTransparent wrapperFormInherit wrapperFormNothing formsRight FormaFull" >
-              <input type="radio" value="Conventional"><span>Conventional</span>
-              <input type="radio" value="FHA" checked><span>FHA</span>
-              <input type="radio" value="VA"><span>VA</span>
-              <input type="radio" value="CASH"><span>CASH</span>
+            <div class="wrapperFormText">
+                <legend class="legend">Loan amount</legend>
+                <input type="text" class="" name="earnMoney" value="" placeholder="$"/>
+            </div>
+        </form>
+        <div class="cleaner"></div>
+          <div class="wrapperForm wrapperFormRadio" >
+              <form action="index.html" method="post" class="formsLeft">
+                  <input type="radio" name="choice" value="Conventional"><span>Conventional</span>
+                  <input type="radio" name="choice" value="FHA" ><span>FHA</span>
+                  <input type="radio" name="choice" value="VA"><span>VA</span>
+                  <input type="radio" name="choice" value="CASH"><span>CASH</span>
+              </form>
           </div>
-            </form>
-          <div class="wrapperForm wrapperFormTransparent wrapperFormNothing wrapperFormText formDatee" id="fixedForm">
+          <div class="wrapperForm wrapperFormText formsLeft" id="fixedForm">
             <form action="" method="post">
                 <legend class="legend">Close of escrow</legend>
                 <input type="date" name="date"class="date" placeholder="Pick a date">
             </form>
           </div>
+          
           <div class="wrapperForm wrapperFormTransparent wrapperFormNothing wrapperFormText formDatee" >
             <form action="" method="post">
                 <legend class="legend legend2">Acceptance deadline</legend>
@@ -269,7 +267,7 @@
       <div id="wrapperFooter" class="">
         <h3>Make an offer</h3>
         <div id="footerIcons">
-          <hr/>
+          
         </div>
         <div class="cleaner"></div>
       </div>
@@ -279,44 +277,43 @@
       <div class="wrapperMid smallerH1">
         <h1>Contingecies</h1>
       </div>
-      <div class="radioForms">
-        <div class="radioForm">
+      <div class="radioForms wrapperForm">
+        <div class="radioForm ">
           <h5>Appraisal</h5>
           <form action="" method="post">
-            <input type="radio" name="" id="">Yes
-            <input type="radio" name="" id="" checked>No
+            <input type="radio" name="choice" id="">Yes
+            <input type="radio" name="choice" id="" checked>No
           </form>
         </div>
-        <div class="radioForm radioFormRight">
+        <div class="radioForm  radioFormRight">
           <h5>Home inspection</h5>
           <form action="" method="post">
-            <input type="radio" name="" id="">Yes
-            <input type="radio" name="" id="" checked>No
+            <input type="radio" name="choice" id="">Yes
+            <input type="radio" name="choice" id="" checked>No
           </form>
         </div>
         <div class="cleaner"></div>
       </div>
-      <div class="radioForms">
+      <div class="radioForms wrapperForm">
         <div class="radioForm">
           <h5>Sale of another property</h5>
           <form action="" method="post">
-            <input type="radio" name="" id="" checked>Yes
-            <input type="radio" name="" id="" >No
+            <input type="radio" name="choice" id="" checked>Yes
+            <input type="radio" name="choice" id="" >No
           </form>
         </div>
         <div class="radioForm radioFormRight">
           <h5>Is offer "as is"</h5>
           <form action="" method="post">
-            <input type="radio" name="" id="" checked>Yes
-            <input type="radio" name="" id="" >No
+            <input type="radio" name="choice" id="" checked>Yes
+            <input type="radio" name="choice" id="" >No
           </form>
         </div>
-        <div class="cleaner"></div>
       </div>
-      <div class="radioForms textForm">
+      <div class="radioForms ">
         <h5>Other terms</h5>
         <form action="" method="post">
-          <input type="text" name="" id="" placeholder="Type here...">
+          <input type="text" name="" id="" class="textForm" placeholder="Type here...">
         </form>
       </div>
       <div class="buttons">
@@ -328,7 +325,6 @@
       <div id="wrapperFooter" class="">
         <h3>Make an offer</h3>
         <div id="footerIcons">
-          <hr/>
         </div>
         <div class="cleaner"></div>
       </div>
@@ -350,7 +346,7 @@
     <div id="wrapperFooter" class="">
         <h3>Make an offer</h3>
         <div id="footerIcons">
-          <hr/>
+          
         </div>
         <div class="cleaner"></div>
     </div>
@@ -437,8 +433,8 @@
        <h1>Success! Thank you for submitting your offer for</h1>
        <h2>11249 E Sonrisa Ave</h2>
        <h5>Mesa, AZ 85212</h5>
-       <h6>Please check your inbox for conformation email.</h6>
-       <h6>Please feel free to contact me anytime.</h6>
+       <h3>Please check your inbox for conformation email.</h3>
+       <h3>Please feel free to contact me anytime.</h3>
       </div>
       <div class="personBlock">
         <div class="iconBlock"></div>
@@ -460,24 +456,21 @@
    <div class="wrapper" id="FirstPage">
       <div class="wrapperMid">
         <h1>Welcome!</h1>
-        <h6>Let's make an offer on</h6>
+        <h3>Let's make an offer on</h3>
         <h2>1249 E Sonrisa Ave</h2>
-        <h6>Mesa, AZ 85212</h6>
+        <h3>Mesa, AZ 85212</h3>
       </div>
-      <div class="wrapperForm wrapperFormTransparent wrapperFormNothing">
+      <div class="wrapperForm  ">
         <form class="" action="#" method="post">
-          <legend class="legend legend3">Submitting agent</legend>
-          <div class="wrapperFormText">
-            <input type="text" name="Name" class="input1" value="Strahinja" placeholder="Name" />
-            </div>
-            <div class="wrapperFormText">
-            <input type="text" name="Last Name" class="input2" value="" placeholder="Last name" />
-            <div class="cleaner">
-            </div>
+          <div class="wrapperFormText formInput">
+            <input type="text" name="Name" class="formMR" value="Strahinja" placeholder="Name" />
           </div>
-          <input type="number" name="phone" class="formFullWidth" id="phone" value="" placeholder="Phone">
+          <div class="wrapperFormText">
+            <input type="text" name="Last Name" class="" value="" placeholder="Last name" />
+          </div>
+          <input type="text" class="formFullWidth formMR" name="" value="" placeholder="Phone"/>
           <input type="email" class="formFullWidth" name="" value="" placeholder="Email"/>
-          <input type="submit" class="formFullWidth formSubmit" name="submit" value="Next &nbsp; &rightarrow;">
+          <input type="submit" class="formSubmitFull" name="submit" value="Submit &rightarrow;">
         </form>
       </div>
       <div class="personBlock">
