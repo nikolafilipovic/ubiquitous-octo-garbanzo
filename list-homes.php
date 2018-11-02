01c96330453b62462d45f80957c92cba95347697
Olja <olja.ivkovic97@gmail.com>
2018-11-02 14:04:50 +0100

    finish the section with address and price

--- list-homes.php
+++ list-homes.php
@@ -6,20 +6,32 @@ Template Name: List Homes
 ?>
 <div class="lh-wrapper">
   <div class="container-fluid">
-    <header class="go-back b-shadow">
-      <i class="fas fa-chevron-left fasback"></i>
-      <a href="#">Back</a>
-    </header>
+      <header class="modal-menu b-shadow">
+        <i class="fas fa-chevron-left fasback"></i>
+        <a href="#">Back</a>
 
-    <figure class="lh-img">
-        <img src="/wp-content/themes/theoffercompany/images/property-1.png" alt="Propery-1">
-      <figcaption>
-        <span>Virtual tour</span>
-        <span>1 / 27</span>
-      </figcaption>
-    </figure> 
+        <!-- make an offer, schedule tour -->
+        <!-- save, share, virtual tour, remove, X -->
+      </header>
 
-    <div class="offer-link-group b-shadow">
+    <div class="row no-gutters">
+      <div class="col-12 col-md-7">
+        <figure class="lh-img-holder">
+          <img src="/wp-content/themes/theoffercompany/images/pexels-photo-259593.jpeg" alt="Propery-1">
+          <figcaption class="d-md-none">
+            <span>Virtual tour</span>
+            <span>1 / 27</span> 
+            <!-- to be hidden in desktop mode -->
+          </figcaption> 
+        </figure>
+      </div>
+
+      <div class="col-12 col-md-5 d-none d-md-block more-imgs">
+        <!-- 4 imgs -->
+      </div>
+    </div>
+
+    <div class="offer-link-group b-shadow d-block d-md-none">
       <div class="offer-link">
         <a href="#">Make an offer</a>
         <i class="fas fa-arrow-right fasarrow"></i>
@@ -31,7 +43,36 @@ Template Name: List Homes
       </div>
     </div>
 
+    <div class="row no-gutters lh-info-wrapper">
+      <div class=" col-12 col-md-9">
+        <div class="row no-gutters">
+          <div class="col-12 col-md-7">
+            <h1 class="lh-info-imp">11249 E Sonrisa Ave</h1>
+            <p>Mesa, AZ 85212</p>
+            <span>3 bds </span> 
+            <span class="small-dot">&#8226;</span> 
+            <span>2 ba </span> 
+            <span class="small-dot">&#8226;</span> 
+            <span>1,209 sqft </span> 
+          </div>
+
+          <div class="col-12 col-md-5">
+            <span class="red-dot"></span>
+            <strong class="small-imp">FOR SALE</strong>
+            <h1 class="lh-info-imp lh-money">$425,000</h1>
+            <a href="#" class="large-link lh-info-imp">Get pre-qualified
+              <i class="fas fa-arrow-right fasarrow"></i>
+            </a>
+          </div>
+        </div>
+        <p class="lh-house">Open House <strong>Fri, Oct 12  &#8226; 11:00am - 2:00pm</strong></p>
+      </div>
+    </div>
+
+
+    <!-- container fluid -->
   </div>
+  <!-- lh-wrapper -->
 </div>
 
 <?php get_footer(); ?>
