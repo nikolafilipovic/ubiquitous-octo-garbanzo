8e2af0bdacf63631b1f91ace8e9ad67293b33431
milutinvelisic <44348167+milutinvelisic@users.noreply.github.com>
2018-11-03 14:24:53 +0100

    Changes to html
    prepravljanje

--- make-an-offer.php
+++ make-an-offer.php
@@ -74,92 +74,113 @@
     </div>
     <!-- Novi block -->
     <div class="wrapper" id="pagetwo">
-      <div class="wrapperMid">
-          <h1>Are you working with a real estate agent?</h1>
-      </div>
-      <div class=" wrapperBlocks">
-        <div class="formBlock formBorderTop">
-          <div class="formCircle">
-              <i class="fas fa-check"></i>
+      <div class="container-fluid h-100">
+        <div class="row h-100">
+          <div class="col-12 my-auto" >
+            <div class="wrapperMid">
+                <h1>Are you working with a real estate agent?</h1>
+            </div>
+            <div class=" wrapperBlocks">
+                <div class="formBlock formBorderTop">
+                  <div class="formCircle">
+                      <i class="fas fa-check"></i>
+                  </div>
+                  <h4>Yes</h4>
+                  <div class="formArrow">
+                    <i class="fa fa-arrow-right" aria-hidden="true"></i>
+                  </div>
+                </div>
+                <div class="formBlock formBorderBot">
+                  <div class="formCircle">
+                      <i class="fas fa-times faml"></i>
+                  </div>
+                  <h4>No</h4>
+                  <div class="formArrow">
+                    <i class="fa fa-arrow-right" aria-hidden="true"></i>
+                  </div>
+                </div>
+            </div>
           </div>
-          <h4>Yes</h4>
-          <div class="formArrow">
-            <i class="fa fa-arrow-right" aria-hidden="true"></i>
-          </div>
-        </div>
-        <div class="formBlock formBorderBot">
-          <div class="formCircle">
-              <i class="fas fa-times faml"></i>
-          </div>
-          <h4>No</h4>
-          <div class="formArrow">
-            <i class="fa fa-arrow-right" aria-hidden="true"></i>
-          </div>
-        </div>
+        </div>  
       </div>
     </div>
     <!-- Novi block -->
     <div class="wrapper" id="pagethree">
-      <div class="wrapperMid">
-          <h1>We can write up the offer for you.</h1>
-        </div>
-        <div class="wrapperMid wrapperWider">
-          <h3>Please provide the informaiton below and Annete will contact you within 5 minutes.</h3>
+      <div class="container-fluid h-100">
+        <div class="row h-100">
+            <div class="col-12 my-auto">
+                <div class="wrapperMid">
+                  <h1>We can write up the offer for you.</h1>
+                </div>
+                <div class="wrapperMid wrapperWider">
+                  <h3>Please provide the informaiton below and Annete will contact you within 5 minutes.</h3>
+                </div>
+                <div class="wrapperForm ">
+                  <form class="" action="#" method="post">
+                    <div class="wrapperFormText formInput">
+                      <input type="text" name="Name" class="formMR" value="Strahinja" placeholder="Name" />
+                    </div>
+                    <div class="wrapperFormText">
+                      <input type="text" name="Last Name" class="" value="" placeholder="Last name" />
+                    </div>
+                    <input type="text" class="formFullWidth formMR" name="" value="" placeholder="Phone"/>
+                    <input type="email" class="formFullWidth" name="" value="" placeholder="Email"/>
+                    <input type="submit" class="formSubmitFull" name="submit" value="Submit &rightarrow;">
+                  </form>
+                </div>
+                <div class="personBlock">
+                    <div class="iconBlock"></div>
+                    <div class="infoBlock">
+                      <h6>Anette Brown</h6>
+                      <i class="fa fa-star" aria-hidden="true"></i>
+                      <i class="fa fa-star" aria-hidden="true"></i>
+                      <i class="fa fa-star" aria-hidden="true"></i>
+                      <i class="fa fa-star" aria-hidden="true"></i>
+                      <i class="fa fa-star" aria-hidden="true"></i>
+                      <p>602-448-7377</p>
+                      <span>anette@theofferco.com</span>
+                    </div>
+                    <div class="cleaner"></div>
+                </div>
+                <div class="cleaner"></div> 
+            </div>  
+        </div> 
       </div>
-      <div class="wrapperForm ">
-        <form class="" action="#" method="post">
-          <div class="wrapperFormText formInput">
-            <input type="text" name="Name" class="formMR" value="Strahinja" placeholder="Name" />
-          </div>
-          <div class="wrapperFormText">
-            <input type="text" name="Last Name" class="" value="" placeholder="Last name" />
-          </div>
-          <input type="text" class="formFullWidth formMR" name="" value="" placeholder="Phone"/>
-          <input type="email" class="formFullWidth" name="" value="" placeholder="Email"/>
-          <input type="submit" class="formSubmitFull" name="submit" value="Submit &rightarrow;">
-        </form>
-      </div>
-      <div class="personBlock">
-        <div class="iconBlock"></div>
-        <div class="infoBlock">
-          <h6>Anette Brown</h6>
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <p>602-448-7377</p>
-          <span>anette@theofferco.com</span>
-        </div>
-        <div class="cleaner"></div>
-      </div>
-        <div class="cleaner"></div>
     </div>
     <!-- Novi block -->
     <div class="wrapper" id="pagefour">
-      <div class="wrapperMid">
-        <h1>Success!</h1>
-        <h3>Anette Brown will contact you shortly.</h3>
-      </div>
-      <div class="personBlock">
-        <div class="iconBlock"></div>
-        <div class="infoBlock">
-          <h6>Anette Brown</h6>
-          <p class="infoblockName">Licensed agent</p>
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <p>602-448-7377</p>
-          <span>anette@theofferco.com</span>
-        </div>
-        <div class="cleaner"></div>
+      <div class="container-fluid h-100">
+        <div class="row h-100">
+            <div class="col-12 my-auto">
+                <div class="wrapperMid">
+                  <h1>Success!</h1>
+                  <h3>Anette Brown will contact you shortly.</h3>
+                </div>
+              <div class="personBlock">
+                <div class="iconBlock"></div>
+                <div class="infoBlock">
+                  <h6>Anette Brown</h6>
+                  <p class="infoblockName">Licensed agent</p>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <p>602-448-7377</p>
+                  <span>anette@theofferco.com</span>
+                </div>
+                <div class="cleaner"></div>
+              </div>
+            </div>  
+        </div>  
       </div>
     </div>
     <!-- Novi block -->
     <div class="wrapper" id="pagefive">
-      <div class="wrapperMid">
+      <div class="container-fluid h-100">
+        <div class="row h-100">
+            <div class="col-12 my-auto">
+                <div class="wrapperMid">
         <h1>Invite your agent</h1>
         <h3>We'll send your agent a link with your request to make an offer on this property.</h3>
       </div>
@@ -187,15 +208,24 @@
           <button  class="formSubmitFull " type="submit">Submit<i class="fa fa-arrow-right" aria-hidden="true"></i></button>
         </form>
       </div>
+            </div>  
+        </div>  
+      </div>
     </div>
     <!-- Novi block -->
     <div class="wrapper" id="pagesix">
-      <div class="wrapperMid">
-        <h1>Success!</h1>
-        <h3>An email has been sent to your agent.</h3>
-      </div>
-      <div class="goBack">
-        <p><a href="#"><i class="fa fa-angle-left" aria-hidden="true"></i>Go back</a></p>
+      <div class="container-fluid h-100">
+        <div class="row h-100">
+            <div class="col-12 my-auto">
+                <div class="wrapperMid">
+                    <h1>Success!</h1>
+                    <h3>An email has been sent to your agent.</h3>
+                </div>
+                <div class="goBack">
+                   <p><a href="#"><i class="fa fa-angle-left" aria-hidden="true"></i>Go back</a></p>
+                </div>
+            </div>  
+        </div>  
       </div>
     </div>
     <!-- Novi block -->
@@ -244,83 +274,95 @@
     </div>
     <!-- novi block -->
     <div class="wrapper" id="pageeight">
-      <div class="wrapperMid smallerH1">
-        <h1>Contingecies</h1>
-      </div>
-      <div class="radioForms wrapperForm radioFormsVertical">
-        <div class="radioForm radioBlock">
-            <h5>Appraisal</h5>
-          <form action="" method="post">
-            <input type="radio" name="choice" id="" ><p>Yes</p>
-            <input type="radio" name="choice" id="" ><p>No</p>
-          </form> 
-        </div>
-        <div class="radioForm radioBlock radioBlock1">
-            <h5>Home inspection</h5>
-          <form action="" method="post" class="formSmall">
-            <input type="radio" name="choice" id="" ><p>Yes</p>
-            <input type="radio" name="choice" id="" ><p>No</p>
-          </form>
-        </div>
-        <div class="radioForm radioBlock">
-            <h5>Sale of another property</h5>
-          <form action="" method="post">
-            <input type="radio" name="choice" id="" ><p>Yes</p>
-            <input type="radio" name="choice" id="" ><p>No</p>
-          </form>
-        </div>
-        <div class="radioForm radioBlock radioBlock1">
-            <h5>Is offer "as is"</h5>
-          <form action="" method="post" class="formSmall">
-            <input type="radio" name="choice" id="" ><p>Yes</p>
-            <input type="radio" name="choice" id="" ><p>No</p>
-          </form>
-        </div>
-        <div class="cleaner"></div>
-      </div>
-      <div class="radioForms">
-        <h5>Other terms</h5>
-        <form action="" method="post">
-          <input type="text" name="" id="" class="textForm" placeholder="Type here...">
-        </form>
-      </div>
-      <div class="buttons">
-        <form action="" method="post">
-          <button  class="formSubmitFull " type="submit"><i class="fa fa-arrow-left" aria-hidden="true"></i>Back</button>
-          <button  class="formSubmitFull " type="submit">Next<i class="fa fa-arrow-right" aria-hidden="true"></i></button>
-        </form>
-      </div>
-      <div id="wrapperFooter" class="">
-        <h3>Make an offer</h3>
-        <div id="footerIcons"></div>
-        <div class="cleaner"></div>
+      <div class="container-fluid h-100">
+        <div class="row h-100">
+            <div class="col-12 my-auto">
+                <div class="wrapperMid smallerH1">
+                    <h1>Contingecies</h1>
+                </div>
+                <div class="radioForms wrapperForm radioFormsVertical">
+                <div class="radioForm radioBlock">
+                    <h5>Appraisal</h5>
+                  <form action="" method="post">
+                    <input type="radio" name="choice" id="" ><p>Yes</p>
+                    <input type="radio" name="choice" id="" ><p>No</p>
+                  </form> 
+                </div>
+                <div class="radioForm radioBlock radioBlock1">
+                    <h5>Home inspection</h5>
+                  <form action="" method="post" class="formSmall">
+                    <input type="radio" name="choice" id="" ><p>Yes</p>
+                    <input type="radio" name="choice" id="" ><p>No</p>
+                  </form>
+                </div>
+                <div class="radioForm radioBlock">
+                    <h5>Sale of another property</h5>
+                  <form action="" method="post">
+                    <input type="radio" name="choice" id="" ><p>Yes</p>
+                    <input type="radio" name="choice" id="" ><p>No</p>
+                  </form>
+                </div>
+                <div class="radioForm radioBlock radioBlock1">
+                    <h5>Is offer "as is"</h5>
+                  <form action="" method="post" class="formSmall">
+                    <input type="radio" name="choice" id="" ><p>Yes</p>
+                    <input type="radio" name="choice" id="" ><p>No</p>
+                  </form>
+                </div>
+                <div class="cleaner"></div>
+              </div>
+              <div class="radioForms">
+                <h5>Other terms</h5>
+                <form action="" method="post">
+                  <input type="text" name="" id="" class="textForm" placeholder="Type here...">
+                </form>
+              </div>
+              <div class="buttons">
+                <form action="" method="post">
+                  <button  class="formSubmitFull " type="submit"><i class="fa fa-arrow-left" aria-hidden="true"></i>Back</button>
+                  <button  class="formSubmitFull " type="submit">Next<i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+                </form>
+              </div>
+            </div>
+            <div id="wrapperFooter" class="">
+                <h3>Make an offer</h3>
+                <div id="footerIcons"></div>
+                <div class="cleaner"></div>
+            </div>
+        </div>  
       </div>
     </div>
     <!-- novi block -->
     <div class="wrapper" id="pagenine">
-      <div class="wrapperMid ">
-        <h1>Offer documents</h1>
-        <p>Please upload purchase contract and proof of funds or pre qualification letter</p>
+      <div class="container-fluid h-100">
+        <div class="row h-100">
+            <div class="col-12 my-auto">
+                <div class="wrapperMid ">
+                    <h1>Offer documents</h1>
+                    <p>Please upload purchase contract and proof of funds or pre qualification letter</p>
+                </div>
+                <div class="Blokovi">
+                  <p>PurchaseContract.pdf</p>
+                  <i class="fas fa-times"></i>
+                </div>
+                <div class="Blokovi BlokoviNC">
+                  <p>Upload file</p>
+                  <i class="fas fa-upload"></i>
+                </div>
+                <div class="buttons">
+                    <form action="" method="post">
+                      <button  class="formSubmitFull " type="submit"><i class="fa fa-arrow-left" aria-hidden="true"></i>Back</button>
+                      <button  class="formSubmitFull " type="submit">Next<i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+                    </form>
+                </div>
+            </div>
+            <div id="wrapperFooter" class="">
+                <h3>Make an offer</h3>
+                <div id="footerIcons"></div>
+                <div class="cleaner"></div>
+            </div>
+        </div>  
       </div>
-      <div class="Blokovi">
-          <p>PurchaseContract.pdf</p>
-          <i class="fas fa-times"></i>
-        </div>
-      <div class="Blokovi BlokoviNC">
-          <p>Upload file</p>
-          <i class="fas fa-upload"></i>
-        </div>
-    <div class="buttons">
-        <form action="" method="post">
-          <button  class="formSubmitFull " type="submit"><i class="fa fa-arrow-left" aria-hidden="true"></i>Back</button>
-          <button  class="formSubmitFull " type="submit">Next<i class="fa fa-arrow-right" aria-hidden="true"></i></button>
-        </form>
-    </div>
-    <div id="wrapperFooter" class="">
-        <h3>Make an offer</h3>
-        <div id="footerIcons"></div>
-        <div class="cleaner"></div>
-    </div>
    </div>
    <!-- novi block -->
    <div class="wrapper" id="pageten">
@@ -417,33 +459,42 @@
    </div>
     <!-- novi block -->
    <div class="wrapper" id="lastPage">
-      <div class="wrapperMid">
-       <h1>Success! Thank you for submitting your offer for</h1>
-       <h2>11249 E Sonrisa Ave</h2>
-       <h5>Mesa, AZ 85212</h5>
-       <h3>Please check your inbox for conformation email.</h3>
-       <h3>Please feel free to contact me anytime.</h3>
-      </div>
-      <div class="personBlock">
-        <div class="iconBlock"></div>
-        <div class="infoBlock">
-          <h6>Anette Brown</h6>
-          <p class="infoblockName">Licensed agent</p>
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <p>602-448-7377</p>
-          <span>anette@theofferco.com</span>
-        </div>
-        <div class="cleaner"></div>
-      </div>
-      <div id="button1"><a href="">Close</a></div>
+      <div class="container-fluid h-100">
+        <div class="row h-100">
+            <div class="col-12 my-auto">
+                <div class="wrapperMid">
+                   <h1>Success! Thank you for submitting your offer for</h1>
+                   <h2>11249 E Sonrisa Ave</h2>
+                   <h5>Mesa, AZ 85212</h5>
+                   <h3>Please check your inbox for conformation email.</h3>
+                   <h3>Please feel free to contact me anytime.</h3>
+                </div>
+                <div class="personBlock">
+                    <div class="iconBlock"></div>
+                    <div class="infoBlock">
+                      <h6>Anette Brown</h6>
+                      <p class="infoblockName">Licensed agent</p>
+                      <i class="fa fa-star" aria-hidden="true"></i>
+                      <i class="fa fa-star" aria-hidden="true"></i>
+                      <i class="fa fa-star" aria-hidden="true"></i>
+                      <i class="fa fa-star" aria-hidden="true"></i>
+                      <i class="fa fa-star" aria-hidden="true"></i>
+                      <p>602-448-7377</p>
+                      <span>anette@theofferco.com</span>
+                    </div>
+                    <div class="cleaner"></div>
+                </div>
+                <div id="button1"><a href="">Close</a></div>
+            </div>  
+        </div>  
+     </div>
    </div>
     <!-- novi block -->
    <div class="wrapper" id="FirstPage">
-      <div class="wrapperMid">
+      <div class="container-fluid h-100">
+        <div class="row h-100">
+            <div class="col-12 my-auto">
+                <div class="wrapperMid">
         <h1>Welcome!</h1>
         <h3>Let's make an offer on</h3>
         <h2>1249 E Sonrisa Ave</h2>
@@ -477,6 +528,9 @@
           <span>anette@theofferco.com</span>
         </div>
         <div class="cleaner"></div>
+      </div>
+            </div>
+        </div> 
       </div>
    </div>
 </div> <!-- /WRAPPER ID-->
