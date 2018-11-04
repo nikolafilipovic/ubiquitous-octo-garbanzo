67f7ffc4f5596e109c0d9a96607eab1aff9e806a
Olja <olja.ivkovic97@gmail.com>
2018-11-04 02:01:29 +0100

    add fontawesome option links and several minor changes

--- list-homes.php
+++ list-homes.php
@@ -6,13 +6,13 @@ Template Name: List Homes
 ?>
 <div class="lh-wrapper">
   <div class="container-fluid">
-      <header class="modal-menu b-shadow">
-        <i class="fas fa-chevron-left fasback"></i>
-        <a href="#">Back</a>
+    <header class="modal-menu b-shadow">
+      <i class="fas fa-chevron-left fasback"></i>
+      <a href="#">Back</a>
 
-        <!-- make an offer, schedule tour -->
-        <!-- save, share, virtual tour, remove, X -->
-      </header>
+      <!-- make an offer, schedule tour -->
+      <!-- save, share, virtual tour, remove, X -->
+    </header>
 
     <div class="row no-gutters">
       <div class="col-12 col-md-7">
@@ -21,12 +21,12 @@ Template Name: List Homes
           <figcaption class="d-md-none">
             <span>Virtual tour</span>
             <span>1 / 27</span>
-          </figcaption> 
+          </figcaption>
         </figure>
       </div>
 
       <div class="col-12 col-md-5 d-none d-md-block more-imgs">
-        <div class="row no-gutters lh-pictures">  
+        <div class="row no-gutters lh-pictures">
           <div class="col-6">
             <div class="image" style="background: url('/wp-content/themes/theoffercompany/images/item.png')"></div>
           </div>
@@ -61,23 +61,23 @@ Template Name: List Homes
           <div class="col-12 col-md-7">
             <h1 class="lh-info-imp">11249 E Sonrisa Ave</h1>
             <p>Mesa, AZ 85212</p>
-            <span>3 bds </span> 
-            <span class="small-dot">&#8226;</span> 
-            <span>2 ba </span> 
-            <span class="small-dot">&#8226;</span> 
-            <span>1,209 sqft </span> 
+            <span>3 bds </span>
+            <span class="small-dot">&#8226;</span>
+            <span>2 ba </span>
+            <span class="small-dot">&#8226;</span>
+            <span>1,209 sqft </span>
+
 
             <div class="d-none d-md-block lh-house-details">
-              <p class="lh-house lh-house--desktop">Open House <strong>Fri, Oct 12  &#8226; 11:00am - 2:00pm</strong></p>
+              <p class="lh-house lh-house--desktop">Open House <strong>Fri, Oct 12 &#8226; 11:00am - 2:00pm</strong></p>
               <i class="fas fa-phone agent-phone"></i><strong class="agent-phone-text">Call Agent</strong>
               <i class="fas fa-comment-alt agent-text"></i><strong class="agent-phone-text">Text Agent</strong>
             </div>
           </div>
-
           <div class="col-12 col-md-5">
             <div class="row no-gutters">
-              <div class="col-6 col-md-12">  
-              <span class="red-dot"></span>
+              <div class="col-6 col-md-12">
+                <span class="red-dot"></span>
                 <strong class="small-imp">FOR SALE</strong>
                 <h1 class="lh-info-imp lh-money">$425,000</h1>
                 <a href="#" class="large-link lh-info-imp">Get pre-qualified
@@ -89,9 +89,45 @@ Template Name: List Homes
               </div>
             </div>
           </div>
+
+          <p class="lh-house d-md-none">Open House <strong>Fri, Oct 12 &#8226; 11:00am - 2:00pm</strong></p>
         </div>
-        <p class="lh-house d-md-none">Open House <strong>Fri, Oct 12  &#8226; 11:00am - 2:00pm</strong></p>
+
+        <div class="faw-options d-block d-md-none">
+          <div class="row no-gutters">
+            <div class="col-3">
+              <i class="far fa-heart"></i>
+              <p>Save</p>
+            </div>
+
+            <div class="col-3">
+              <i class="fas fa-share-alt"></i>
+              <p>Share</p>
+            </div>
+
+            <div class="col-3">
+              <i class="fas fa-vr-cardboard"></i>
+              <p>Virtual Tour</p>
+            </div>
+
+            <div class="col-3">
+              <i class="far fa-times-circle"></i>
+              <p>Remove</p>
+            </div>
+
+          </div>
+        </div>
+
+        <div class="lh-house-details">
+        <i class="fas fa-phone agent-phone"></i>
+      <strong class="agent-phone-text">Call Agent</strong>
+      <i class="fas fa-comment-alt agent-text"></i>
+      <strong class="agent-phone-text">Text Agent</strong>
+        </div>
+
+
       </div>
+
       <div class="col-12 col-md-4 contact-agent">
         <h1 class="contact-agent-title">Contact Agent</h1>
         <div class="contact-agent-prev row no-gutters">
@@ -114,46 +150,59 @@ Template Name: List Homes
           </div>
         </div>
         <form class="contact-agent-form">
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <div class="input-group-text">
-                  <i class="fas fa-user"></i>
-                </div>
-              </div>
-              <input type="text" class="form-control" placeholder="Name" />
-            </div>
-
-            <div class="input-group">
-              <div class="shader"></div>
-              <div class="input-group-prepend">
-                <div class="input-group-text">
-                  <i class="fas fa-phone"></i>
-                </div>
-              </div>
-              <input type="text" class="form-control" placeholder="Phone" />
-            </div>
-
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <div class="input-group-text">
-                  <i class="fas fa-envelope"></i>
-                </div>
-              </div>
-              
-              <input type="email" class="form-control" placeholder="Email" />
-            </div>
-
-            <div class="input-group">
-              <textarea class="form-control" placeholder="Question"></textarea>
-            </div>
-
-             <div class="form-check d-flex justify-content-center">
-              <div>
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">I want financing information</label>
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <div class="input-group-text">
+                <i class="fas fa-user"></i>
               </div>
             </div>
+            <input type="text" class="form-control" placeholder="Name" />
+          </div>
+
+          <div class="input-group">
+            <div class="shader"></div>
+            <div class="input-group-prepend">
+              <div class="input-group-text">
+                <i class="fas fa-phone"></i>
+              </div>
+            </div>
+            <input type="text" class="form-control" placeholder="Phone" />
+          </div>
+
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <div class="input-group-text">
+                <i class="fas fa-envelope"></i>
+              </div>
+            </div>
+
+            <input type="email" class="form-control" placeholder="Email" />
+          </div>
+
+          <div class="input-group">
+            <textarea class="form-control" placeholder="Question"></textarea>
+          </div>
+
+          <div class="form-check d-flex justify-content-center">
+            <div>
+              <input type="checkbox" class="form-check-input" id="exampleCheck1">
+              <label class="form-check-label" for="exampleCheck1">I want financing information</label>
+            </div>
+          </div>
+
+          <div class="offer-link">
+            <button>Contact Agent</button>
+            <i class="fas fa-arrow-right fasarrow"></i>
+          </div>
         </form>
+
+        <div class="another-contact">
+          <img src="/wp-content/themes/theoffercompany/images/man.png" />
+          <div class="preferred-lender">
+            <p>Preferred lender</p>
+            <h2 class="contact-agent-prev-name">David Houze</h2>
+          </div>
+        </div>
       </div>
     </div>
     <!-- container fluid -->
