8c5c420ae8988874f41552eb7d676cc692a89152
Olja <olja.ivkovic97@gmail.com>
2018-11-18 01:30:34 +0100

    make floating modal

--- find-my-local-pro.php
+++ find-my-local-pro.php
@@ -7,6 +7,10 @@ Template Name: Finy my Local Pro
 
 <div class="fmlp-header-container">
   <div class="faded">
+    <a href="#" class="go-back">
+      <i class="far fa-chevron-left"></i>
+      Back
+    </a>
     <div class="fmlp-header">
       <img src="/wp-content/themes/theoffercompany/images/logo.png" alt="logo">
       <h1>Find my pro</h1>
@@ -14,15 +18,37 @@ Template Name: Finy my Local Pro
         <div class="landing-search">
           <div class="search-field">
             <div class="input-group buy" id="input-group">
-              <input type="text" class="form-control" id="landing-search" placeholder="Enter address, neighborhood, city or zip...">
+              <input type="text" class="form-control" id="landing-search" placeholder="Enter your zip code">
               <div class="input-group-prepend">
                 <div class="input-group-text"><i class="fas fa-map-marker-alt"></i></div>
               </div>
             </div>
           </div>
         </div>
-        <a href="#" class="offer-link">Submit</a>
+        <a href="#" class="offer-link">
+          Submit
+          <i class="far fa-arrow-right"></i>
+        </a>
       </div>
+    </div>
+  </div>
+</div>
+
+<div class="fmlp-body">
+  <div class="fmlp-floating-block">
+    <div class="one-row">
+      <h2>Service from start to finish</h2>
+      <p>Each Offer Company Agent is supported by a team so you'll get on-demand service from touring to close</p>
+    </div>
+
+      <div class="one-row">
+      <h2>Expert negotiators</h2>
+      <p>Each Offer Company Agent is supported by a team so you'll get on-demand service from touring to close</p>
+    </div>
+
+      <div class="one-row">
+      <h2>We have your back</h2>
+      <p>Each Offer Company Agent is supported by a team so you'll get on-demand service from touring to close</p>
     </div>
   </div>
 </div>
