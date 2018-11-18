4635185e9c01c173f9821eb9dd92aa6400e371e5
Olja <olja.ivkovic97@gmail.com>
2018-11-18 16:21:55 +0100

    fix responsivity of fmlp

--- find-my-local-pro.php
+++ find-my-local-pro.php
@@ -7,12 +7,14 @@ Template Name: Finy my Local Pro
 
 <div class="fmlp-header-container">
   <div class="faded">
-    <a href="#" class="go-back">
-      <i class="far fa-chevron-left"></i>
-      Back
-    </a>
+    <div class="link-container">
+      <a href="#" class="go-back">
+        <i class="far fa-chevron-left"></i>
+        Back
+      </a>
+    </div>
     <div class="fmlp-header">
-      <img src="/wp-content/themes/theoffercompany/images/logo.png" alt="logo">
+      <img src="/wp-content/themes/theoffercompany/images/logo.png" class="d-none d-md-block" alt="logo">
       <h1>Find my pro</h1>
       <div class="enterzip-group">
         <div class="enterzip">
@@ -33,29 +35,33 @@ Template Name: Finy my Local Pro
         </div>
       </div>
     </div>
+
   </div>
 </div>
 
 <div class="fmlp-body">
-  <div class="fmlp-floating-block">
-    <div class="one-row">
-      <h2>Service from start to finish</h2>
-      <p>Each Offer Company Agent is supported by a team so you'll get on-demand service from touring to close</p>
+  <div class="fmlp-floating-container">
+    <div class="fmlp-floating-block">
+      <div class="one-row">
+        <h2>Service from start to finish</h2>
+        <p>Each Offer Company Agent is supported by a team so you'll get on-demand service from touring to close</p>
+      </div>
+
+      <div class="one-row">
+        <h2>Expert negotiators</h2>
+        <p>Each Offer Company Agent is a local expert. Our agents use local knowledge and proprietary data to negotiate
+          the best deal for you.</p>
+      </div>
+
+      <div class="one-row">
+        <h2>We have your back</h2>
+        <p>Each Offer Company Agents are customer advocates. We're accountable for helping our clients buy and sell the
+          right home, at the right price.</p>
+      </div>
     </div>
 
-    <div class="one-row">
-      <h2>Expert negotiators</h2>
-      <p>Each Offer Company Agent is a local expert. Our agents use local knowledge and proprietary data to negotiate the best deal for you.</p>
-    </div>
-
-    <div class="one-row">
-      <h2>We have your back</h2>
-      <p>Each Offer Company Agents are customer advocates. We're accountable for helping our clients buy and sell the right home, at the right price.</p>
-    </div>
-  </div>
-
-  <div class="fmlp-floating-block block--smaller">
-    <h1>Find my pro</h1>
+    <div class="fmlp-floating-block block--smaller">
+      <h1>Find my pro</h1>
       <div class="enterzip">
         <div class="landing-search">
           <div class="search-field">
@@ -72,6 +78,7 @@ Template Name: Finy my Local Pro
           <i class="far fa-arrow-right"></i>
         </a>
       </div>
+    </div>
   </div>
 </div>
 
