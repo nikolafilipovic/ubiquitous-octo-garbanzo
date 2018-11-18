7ade7c494e2c19cd99fa187fb24015f935e90e7b
Olja <olja.ivkovic97@gmail.com>
2018-11-18 14:46:23 +0100

    finish desktop version

--- find-my-local-pro.php
+++ find-my-local-pro.php
@@ -14,6 +14,48 @@ Template Name: Finy my Local Pro
     <div class="fmlp-header">
       <img src="/wp-content/themes/theoffercompany/images/logo.png" alt="logo">
       <h1>Find my pro</h1>
+      <div class="enterzip-group">
+        <div class="enterzip">
+          <div class="landing-search">
+            <div class="search-field">
+              <div class="input-group buy" id="input-group">
+                <input type="text" class="form-control" id="landing-search" placeholder="Enter your zip code">
+                <div class="input-group-prepend">
+                  <div class="input-group-text"><i class="fas fa-map-marker-alt"></i></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <a href="#" class="offer-link">
+            Submit
+            <i class="far fa-arrow-right"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="fmlp-body">
+  <div class="fmlp-floating-block">
+    <div class="one-row">
+      <h2>Service from start to finish</h2>
+      <p>Each Offer Company Agent is supported by a team so you'll get on-demand service from touring to close</p>
+    </div>
+
+    <div class="one-row">
+      <h2>Expert negotiators</h2>
+      <p>Each Offer Company Agent is a local expert. Our agents use local knowledge and proprietary data to negotiate the best deal for you.</p>
+    </div>
+
+    <div class="one-row">
+      <h2>We have your back</h2>
+      <p>Each Offer Company Agents are customer advocates. We're accountable for helping our clients buy and sell the right home, at the right price.</p>
+    </div>
+  </div>
+
+  <div class="fmlp-floating-block block--smaller">
+    <h1>Find my pro</h1>
       <div class="enterzip">
         <div class="landing-search">
           <div class="search-field">
@@ -30,26 +72,6 @@ Template Name: Finy my Local Pro
           <i class="far fa-arrow-right"></i>
         </a>
       </div>
-    </div>
-  </div>
-</div>
-
-<div class="fmlp-body">
-  <div class="fmlp-floating-block">
-    <div class="one-row">
-      <h2>Service from start to finish</h2>
-      <p>Each Offer Company Agent is supported by a team so you'll get on-demand service from touring to close</p>
-    </div>
-
-      <div class="one-row">
-      <h2>Expert negotiators</h2>
-      <p>Each Offer Company Agent is supported by a team so you'll get on-demand service from touring to close</p>
-    </div>
-
-      <div class="one-row">
-      <h2>We have your back</h2>
-      <p>Each Offer Company Agent is supported by a team so you'll get on-demand service from touring to close</p>
-    </div>
   </div>
 </div>
 
