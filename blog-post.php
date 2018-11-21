87abb0477974ef461fe6831602d544a53afee73e
Stefan Kupresak <stefan_vg@hotmail.com>
2018-11-21 01:42:31 +0100

    finsih mobile first design of the top part of the page

--- blog-post.php
+++ blog-post.php
@@ -5,6 +5,70 @@ Template Name: Blog Post
   get_header(nomenu);
 ?>
 
-<h1>Kupi srcee <3 </h1>
+<section class="blog-post">
+  <header class="blog-post__wrap">
+    <div class="blog-post__image"></div>
+    <h1 class="blog-post__pre-title">Upgrading Home</h1>
+  </header>
+
+  <article class="container-fluid">
+    <h1 class="blog-post__title">
+      Prep for `Prost!` Season 9 Bavarian Style Homes
+      to Inspire Oktoberfest
+    </h1>
+    <section class="blog-post-sub">
+      <img src="/wp-content/themes/theoffercompany/images/face.png" alt="author" />
+      <h2 class="blog-post__sub-title">
+        Chris Brown <span>on 25 Oct 2018</span>
+      </h2>
+    </section>
+    <section class="sphere-container spaced">
+      <a class="sphere-small sphere--fb">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a class="sphere-small sphere--twitt">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a class="sphere-small sphere--in">
+        <i class="fab fa-linkedin-in"></i>
+      </a>
+      <a class="sphere-small sphere--pinta">
+        <i class="fab fa-pinterest-p"></i>
+      </a>
+      <a class="sphere-small sphere--email">
+        <i class="far fa-envelope"></i>
+      </a>
+    </section>
+    <section class="blog-post__video">
+      <img src="/wp-content/themes/theoffercompany/images/video.png" alt="video" />
+      <a class="sphere-small sphere--email">
+        <i class="fas fa-play"></i>
+      </a>
+      <span class="play">Play</span>
+    </section>
+    <section>
+      <h2>
+        Dust off your lederhosen and steins, because 
+        it's the most wunderbar time of the year!
+      </h2>
+      <p>
+        If you can’t make it to Munich for Oktoberfest, 
+        you can still enjoy a good beer at one 
+        of the American events happening all throughout the country in September.
+      </p>
+      <p>      
+        From Frankenmuth, Michigan, to Fredericksburg, Texas, you’re certain to find 
+        a spirited group of Germanophiles somewhere during the month 
+        celebrating all things Bavarian.
+      </p>
+      <p>
+        To get inspired for Oktoberfest, check out these nine Bavarian-style 
+        homes for sale right here in the U.S.
+      </p>
+    </section>
+  </article>
+</section>
+
+
 
 <?php get_footer(); ?>
